30ec47d5303e780d1d0d68c0f6fe7b7ccbad13c5
Proser-V <10530@holbertonstudents.com>
2025-04-07 22:30:06 +0200

    15-binary_tree_is_full.c rewrite

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,24 +10,14 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int check = 0;
-
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL)
-	{
-		if (tree->right == NULL)
-			return (0);
-	}
-	else if (tree->left == NULL)
-	{
-		if (tree->right != NULL)
-			return (0);
-	return (1);
-	}
-	check = binary_tree_is_full(tree->left);
-	check = binary_tree_is_full(tree->right);
+	if ((tree->left == NULL) && (tree->right == NULL))
+		return (1);
 
-	return (check);
+	if ((tree->left != NULL) && (tree->right != NULL))
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+
+	return (0);
 }
